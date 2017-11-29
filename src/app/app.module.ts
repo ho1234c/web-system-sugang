e6700f55729c3cfb9a239926c3ab548b527682ce
의원 <elang1301@naver.com>
2017-11-29 12:51:24 +0900

    asdf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { MatListModule, MatToolbarModule, MatGridListModule } from '@angular/material';
+//import { SearchSettingComponent } from './searchSetting.component';
+//import { SearchListComponent } from './searchList.component';
+//import { JoinListComponent } from './joinList.component';
+import { ChattingComponent } from './chatting.component';
 import { AppComponent } from './app.component';
 
+//import { SocketIoModule, SocketIoConfig } from 'ng2-socket-io';
+
+//const config: SocketIoConfig = { url: 'http://localhost:4200', options: {} };
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, ChattingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, MatListModule, MatToolbarModule, MatGridListModule,
+    //SocketIoModule.forRoot(config)
   ],
   providers: [],
   bootstrap: [AppComponent]
