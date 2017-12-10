087dda683690fe12cc313c8d3487c3672fc7fcba
의원 <elang1301@naver.com>
2017-12-11 02:25:42 +0900

    add&merge

---
+++ src/app/announce.ts
@@ -0,0 +1,5 @@
+export class Announce {
+  announceId: number;
+  title: string;
+  content: string;
+}
---
+++ src/app/announceComponent/announce.component.ts
@@ -0,0 +1,50 @@
+import {Component, ViewChild} from '@angular/core';
+import {Input} from '@angular/compiler/src/core';
+import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
+import { AnnounceSet } from '../mock.announce';
+import {Announce} from '../announce';
+
+@Component({
+  selector: 'app-announce',
+  templateUrl: './announce.component.html',
+  styleUrls: ['./announce.component.css']
+})
+export class AnnounceComponent {
+
+  announceList = AnnounceSet;
+  @ViewChild('form') myform;
+
+  announceColumns = ['announceId', 'title'];
+  dataSource: MatTableDataSource<Announce>;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor() {
+    const announces: Announce[] = [];
+    const temp = this.announceList.length;
+    for (let i = 0; i < temp; i++) { announces.push(this.announceList.pop()); }
+
+    this.dataSource = new MatTableDataSource(announces);
+  }
+
+  /**
+   * Set the paginator and sort after the view init since this component will
+   * be able to query its view for the initialized paginator and sort.
+   */
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
+  }
+
+  inflate() {
+
+  }
+
+}
